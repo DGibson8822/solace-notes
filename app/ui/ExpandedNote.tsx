ffc888141c32214e8de73c322cdf0a3729f12093
Dominique Gibson <DGibson8822@gmail.com>
2024-05-15 19:44:08 -0500

    Decreased font size of last updated text.

--- app/ui/ExpandedNote.tsx
+++ app/ui/ExpandedNote.tsx
@@ -9,7 +9,9 @@ export default function ExpandedNote({
     <div className="flex flex-col w-auto max-w-96 p-7 bg-gray-50 rounded-lg drop-shadow-md border-2 border-stone-500">
       <div className="flex-1">
         <h2 className="font-bold">{note.title}</h2>
-        <p>Last updated: {note.updated_at.toLocaleString()}</p>
+        <p className="text-sm">
+          Last updated: {note.updated_at.toLocaleString()}
+        </p>
         <p className="m-4">{note.content}</p>
       </div>
       <div className="mt-5 flex">
--- app/ui/NotePreview.tsx
+++ app/ui/NotePreview.tsx
@@ -7,7 +7,9 @@ export default function NotePreview({ note }: { note: Note }) {
     <div className="flex flex-col w-auto max-w-96 h-80  p-7 bg-gray-100 rounded-lg drop-shadow-md border-2 border-stone-500">
       <div className="flex-1">
         <h2 className="font-bold">{note.title}</h2>
-        <p>Last updated: {note.updated_at.toLocaleString()}</p>
+        <p className="text-sm">
+          Last updated: {note.updated_at.toLocaleString()}
+        </p>
         <p className="m-3 h-24 text-ellipsis overflow-hidden ">
           {note.content}
         </p>
